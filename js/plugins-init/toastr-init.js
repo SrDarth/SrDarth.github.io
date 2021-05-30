37a5a88bcc285cd376a2ea43ab55c8edbaa27614
ALEXANDRE MAXIMIANO DE FREITAS <alexandremaximiano72@gmail.com>
2021-05-30 04:40:12 -0300

    pp

--- js/plugins-init/toastr-init.js
+++ js/plugins-init/toastr-init.js
@@ -7,8 +7,8 @@ Toastr
 *******************/
 
     $("#toastr-success-top-right").on("click", function () {
-                toastr.success("This Is Success Message", "Top Right", {
-                    timeOut: 500000000,
+                toastr.success("", "Copiado!", {
+                    timeOut: 5e3,
                     closeButton: !0,
                     debug: !1,
                     newestOnTop: !0,
